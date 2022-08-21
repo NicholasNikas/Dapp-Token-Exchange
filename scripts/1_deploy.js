0b70edd4e0e4a600451893161bb77000d653744f
Nicholas Nikas <nicholasnikas@Nicholass-MacBook-Pro.local>
2022-08-21 18:59:41 -0400

    2. Smoke Test

---
+++ scripts/1_deploy.js
@@ -0,0 +1,18 @@
+const { ethers } = require('hardhat')
+
+async function main() {
+  // Fetch contract to deploy
+  const Token = await ethers.getContractFactory('Token')
+
+  // Deploy contract
+  const token = await Token.deploy()
+  await token.deployed()
+  console.log(`Token Deployed to: ${token.address}`)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exitCode = 1
+  })
