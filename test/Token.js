e8d6b833f4bd321cba6be91a01dd42b1f1e342e9
Nicholas Nikas <nicholasnikas@Nicholass-MacBook-Pro.local>
2022-08-21 22:41:12 -0400

    3. Testing Contracts

---
+++ test/Token.js
@@ -0,0 +1,33 @@
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+describe('Token', () => {
+  let token
+  beforeEach(async () => {
+    const Token = await ethers.getContractFactory('Token')
+    token = await Token.deploy('Dapp University', 'DAPP', '1000000')
+  })
+
+  describe('Deployment', () => {
+    const name = 'Dapp University'
+    const symbol = 'DAPP'
+    const decimals = 18
+    const totalSupply = tokens(1000000)
+    it('has correct name', async () => {
+      expect(await token.name()).to.equal(name)
+    })
+    it('has correct symbol', async () => {
+      expect(await token.symbol()).to.equal(symbol)
+    })
+    it('has correct decimals', async () => {
+      expect(await token.decimals()).to.equal(decimals)
+    })
+    it('has correct totalSupply', async () => {
+      expect(await token.totalSupply()).to.equal(totalSupply)
+    })
+  })
+})
