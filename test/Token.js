4248aef1973af3857afe3958a077705fca452308
Nicholas Nikas <nicholasnikas@Nicholass-MacBook-Pro.local>
2022-08-23 19:42:59 -0400

    4. Token Balances

--- test/Token.js
+++ test/Token.js
@@ -6,10 +6,13 @@ const tokens = (n) => {
 }
 
 describe('Token', () => {
-  let token
+  let token, accounts, deployer
+
   beforeEach(async () => {
     const Token = await ethers.getContractFactory('Token')
     token = await Token.deploy('Dapp University', 'DAPP', '1000000')
+    accounts = await ethers.getSigners()
+    deployer = accounts[0]
   })
 
   describe('Deployment', () => {
@@ -20,14 +23,21 @@ describe('Token', () => {
     it('has correct name', async () => {
       expect(await token.name()).to.equal(name)
     })
+
     it('has correct symbol', async () => {
       expect(await token.symbol()).to.equal(symbol)
     })
+
     it('has correct decimals', async () => {
       expect(await token.decimals()).to.equal(decimals)
     })
+
     it('has correct totalSupply', async () => {
       expect(await token.totalSupply()).to.equal(totalSupply)
+    })
+
+    it('assigns totalSupply to deployer', async () => {
+      expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
     })
   })
 })
