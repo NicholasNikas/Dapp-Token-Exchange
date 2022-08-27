61df814896842a17e24b443f88644909b965bf2d
Nicholas Nikas <nicholasnikas@Nicholass-MacBook-Pro.local>
2022-08-27 12:07:51 -0400

    11. Make Orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -8,9 +8,30 @@ contract Exchange {
     address public feeAccount;
     uint256 public feePercent;
     mapping(address => mapping(address => uint256)) public tokens;
+    mapping(uint256 => _Order) public orders;
+    uint256 public orderCount;
+
+    struct _Order {
+        uint256 id;
+        address user;
+        address tokenGet; 
+        uint256 amountGet; 
+        address tokenGive; 
+        uint256 amountGive;
+        uint256 timeStamp;
+    }
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
     event Withdraw(address token, address user, uint256 amount, uint256 balance);
+    event Order(
+        uint256 id,
+        address user,
+        address tokenGet, 
+        uint256 amountGet, 
+        address tokenGive, 
+        uint256 amountGive,
+        uint256 timeStamp
+            );
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
@@ -19,22 +40,47 @@ contract Exchange {
 
     function depositToken(address _token, uint256 _amount) public {
         require(Token(_token).transferFrom(msg.sender, address(this), _amount));
-
         tokens[_token][msg.sender] += _amount;
-        
         emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 
     function withdrawToken(address _token, uint256 _amount) public {
         require(tokens[_token][msg.sender] >= _amount);
-
         Token(_token).transfer(msg.sender, _amount);
         tokens[_token][msg.sender] -= _amount;
-
         emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 
     function balanceOf(address _token, address _user) public view returns (uint256) {
         return tokens[_token][_user];
     }
+
+    // ------------
+    // Make and cancel orders
+
+    function makeOrder(
+        address _tokenGet, 
+        uint256 _amountGet, 
+        address _tokenGive, 
+        uint256 _amountGive
+        ) public {
+            require(balanceOf(_tokenGive, msg.sender) >= _amountGive);
+            orderCount++;
+            orders[orderCount] = _Order(
+                orderCount,
+                msg.sender,
+                _tokenGet,
+                _amountGet,
+                _tokenGive,
+                _amountGive,
+                block.timestamp
+            );
+            emit Order(orderCount,
+                msg.sender,
+                _tokenGet,
+                _amountGet,
+                _tokenGive,
+                _amountGive,
+                block.timestamp);
+        }
 }
--- test/Exchange.js
+++ test/Exchange.js
@@ -6,7 +6,7 @@ const tokens = (n) => {
 }
 
 describe('Exchange', () => {
-  let exchange, accounts, deployer, feeAccount, token1
+  let exchange, accounts, deployer, feeAccount, token1, token2
   const feePercent = 10
 
   beforeEach(async () => {
@@ -14,6 +14,7 @@ describe('Exchange', () => {
     const Token = await ethers.getContractFactory('Token')
 
     token1 = await Token.deploy('Dapp University', 'DAPP', '1000000')
+    token2 = await Token.deploy('Mock Dai', 'mDAI', '1000000')
 
     accounts = await ethers.getSigners()
     deployer = accounts[0]
@@ -150,6 +151,57 @@ describe('Exchange', () => {
       expect(await exchange.balanceOf(token1.address, user1.address)).to.equal(
         amount
       )
+    })
+  })
+
+  describe('Making orders', () => {
+    let transaction, result
+    let amount = tokens(1)
+
+    describe('Success', () => {
+      beforeEach(async () => {
+        transaction = await token1
+          .connect(user1)
+          .approve(exchange.address, amount)
+        result = await transaction.wait()
+
+        transaction = await exchange
+          .connect(user1)
+          .depositToken(token1.address, amount)
+        result = await transaction.wait()
+
+        transaction = await exchange
+          .connect(user1)
+          .makeOrder(token2.address, tokens(1), token1.address, tokens(1))
+        result = await transaction.wait()
+      })
+
+      it('tracks the newly created order', async () => {
+        expect(await exchange.orderCount()).to.equal(1)
+      })
+
+      it('emits a Order event', async () => {
+        const event = result.events[0]
+        expect(event.event).to.equal('Order')
+        const args = event.args
+        expect(args.id).to.equal(1)
+        expect(args.user).to.equal(user1.address)
+        expect(args.tokenGet).to.equal(token2.address)
+        expect(args.amountGet).to.equal(tokens(1))
+        expect(args.tokenGive).to.equal(token1.address)
+        expect(args.amountGive).to.equal(tokens(1))
+        expect(args.timeStamp).to.at.least(1)
+      })
+    })
+
+    describe('Failure', async () => {
+      it('rejects orders with insufficient balance', async () => {
+        await expect(
+          exchange
+            .connect(user1)
+            .makeOrder(token2.address, tokens(1), token1.address, tokens(1))
+        ).to.be.reverted
+      })
     })
   })
 })
