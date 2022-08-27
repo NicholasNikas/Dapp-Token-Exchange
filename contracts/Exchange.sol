080ff0224e49274e850054dd89588efed1fecec0
Nicholas Nikas <nicholasnikas@Nicholass-MacBook-Pro.local>
2022-08-26 23:02:50 -0400

    10. Withdraw Tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -10,19 +10,28 @@ contract Exchange {
     mapping(address => mapping(address => uint256)) public tokens;
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Withdraw(address token, address user, uint256 amount, uint256 balance);
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
     }
 
-    // ------------------------
-    // DEPOSIT & WITHDRAW TOKEN
-
     function depositToken(address _token, uint256 _amount) public {
         require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+
         tokens[_token][msg.sender] += _amount;
+        
         emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    function withdrawToken(address _token, uint256 _amount) public {
+        require(tokens[_token][msg.sender] >= _amount);
+
+        Token(_token).transfer(msg.sender, _amount);
+        tokens[_token][msg.sender] -= _amount;
+
+        emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 
     function balanceOf(address _token, address _user) public view returns (uint256) {
